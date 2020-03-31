42d32b64848e766e850fc89c6d2860d8cd6cb413
S-Au-Zn <43560642+S-Au-Zn@users.noreply.github.com>
2020-03-31 16:15:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> hhh
 '''this is simulation'''
 import numpy as np
 
@@ -9,7 +5,4 @@ A = np.array([1, 2, 3, 4])
 print(A)
 print("hello world")
 
-=======
-print("hello world")
-print("edit in xps")
->>>>>>> 4e29434a4d97d94dd37a06c861088c715b1b51f5
+
