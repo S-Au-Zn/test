6b3a00e303b6b2b14a9c8e4e7030de7fe6b5a020
CS_Lab <601870665@qq.com>
2020-03-31 15:21:04 +0800

    3

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 '''this is simulation'''
 import numpy as np
 
@@ -5,3 +6,7 @@ A = np.array([1, 2, 3, 4])
 print(A)
 print("hello world")
 
+=======
+print("hello world")
+print("edit in xps")
+>>>>>>> 4e29434a4d97d94dd37a06c861088c715b1b51f5
